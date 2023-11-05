89afad680b159608ff12224fc4b17bceb0f61f2a
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-11-05 15:05:07 +0530

    Implement Dark Mode

---
+++ src/components/Nav/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Switcher from "@/components/Nav/Mode/Switcher";
+
+const Navbar = () => {
+    return (
+        <div>
+           <Switcher/>
+        </div>
+    );
+};
+
+export default Navbar;
