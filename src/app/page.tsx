1c7f40efb38bebf8067a21129810c10a27472d53
Subhasish Negel <negelsubho@gmail.com>
2023-11-09 23:09:12 +0530

    Added navbar, Edited Layout, Added Theme Switcher on Logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="bg-white dark:bg-stone-800">
-      <p className="text-xl text-black dark:text-white">
-
-        This is my portfolio website.
-      </p>
-
-    </main>
-  )
+  return <main className=""></main>;
 }
--- src/components/Nav/Mode/Switcher.tsx
+++ src/components/Nav/Mode/Switcher.tsx
@@ -2,6 +2,7 @@
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 import { BsMoonFill, BsFillSunFill } from "react-icons/bs";
+import { MdDeveloperMode } from "react-icons/md";
 import { motion } from "framer-motion";
 
 const Switcher = () => {
@@ -21,7 +22,7 @@ const Switcher = () => {
             <motion.button
                 id="theme-btn"
                 aria-label="Toggle Dark Mode"
-                className="text-xl"
+                className="flex justify-center items-center"
                 whileTap={{
                     scale: 0.7,
                     rotate: 360,
@@ -30,7 +31,7 @@ const Switcher = () => {
                 whileHover={{ scale: 1.2 }}
                 onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
             >
-                {theme === "dark" ? <BsMoonFill /> : <BsFillSunFill />}
+                {theme === "dark" ? <MdDeveloperMode className="text-3xl" /> : <MdDeveloperMode className="text-3xl"/>}
             </motion.button>
         </div>
     );
