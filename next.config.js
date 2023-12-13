38c827972443fb7fb4694df445527842cf74b323
Suman Mandal <36975298+suman2mandal@users.noreply.github.com>
2023-12-13 12:49:28 +0530

    Blog post structure created

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['d33wubrfki0l68.cloudfront.net'],
+        domains: ['d33wubrfki0l68.cloudfront.net','cdn.pixabay.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'images.unsplash.com',
+                pathname: '/**',
+            },
+        ],
     },
 }
 
--- src/app/(Blog)/Blog/[id]/page.tsx
+++ src/app/(Blog)/Blog/[id]/page.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import BlogHeader from "@/components/Blog/Banner/BlogHeader";
+import PostBanner from "@/components/Blog/Banner/PostBanner";
 
-function Page() {
+function page() {
     return (
-        <div>
-            Page number 1
+        <div className="flex justify-evenly space-x-6 mx-4">
+            <div className="bg-cyan-700 h-screen z-10 sticky top-0 left-0 w-1/4 p-4">
+a
+            </div>
+            <div className="bg-cyan-800 h-full w-1/2">
+                <PostBanner imageUrl="https://cdn.pixabay.com/photo/2016/09/01/19/53/pocket-watch-1637396_1280.jpg"/>
+                    <BlogHeader/>
+                <BlogHeader/>
+                <BlogHeader/>
+                <BlogHeader/>
+                <BlogHeader/>
+            </div>
+            <div className="bg-cyan-900 h-screen w-1/4 p-4">
+c
+            </div>
         </div>
     );
 }
 
-export default Page;
+export default page;
