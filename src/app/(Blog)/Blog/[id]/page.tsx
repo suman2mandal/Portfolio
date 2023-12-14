f65c2f6967fa313586f5550f7e3166f6b1c82c42
Suman Mandal <36975298+suman2mandal@users.noreply.github.com>
2023-12-14 13:46:03 +0530

    Refactor code for blog sidebar link

--- src/app/(Blog)/Blog/[id]/page.tsx
+++ src/app/(Blog)/Blog/[id]/page.tsx
@@ -1,65 +1,39 @@
+"use client";
 import React from 'react';
 import BlogHeader from "@/components/Blog/Banner/BlogHeader";
 import PostBanner from "@/components/Blog/Banner/PostBanner";
 import Firstpost from "@/components/Blog/Contents/MDX/Firstpost.mdx";
 import Pagination from "@/components/Blog/Pagination/Pagination";
-function page() {
+import SidebarTabLinkProps from "@/components/Blog/Contents/SidebarTabLinkProps";
+import { TbTransitionTop } from "react-icons/tb";
+import Template from "@/components/Blog/Contents/Post/Template";
+import {Link } from "@/components/Blog/Contents/Post/Template";
+
+function Page() {
+    const links: Link[] = [
+        {
+            link_text: "Choosing a Programming Language",
+            indent: 2,
+            jump: "Choosing-a-Programming-Language",
+        },
+        {
+            icon: <TbTransitionTop />,
+            link_text: "Go to Top",
+            indent: 2,
+            jump: "top",
+        }
+    ];
+
     return (
         <div>
-        <div className="flex justify-between md:justify-evenly md:space-x-6 md:mx-28">
-            <div className="bg-slate-600 overflow-scroll scrollbar-hide rounded-2xl h-fit z-10 md:sticky top-0 left-0 w-1/4 mt-4 fixed">
-                <div className="">
-                    <PostBanner imageUrl="https://cdn.pixabay.com/photo/2016/09/01/19/53/pocket-watch-1637396_1280.jpg"/>
-                    <div className="p-4">
-                        <ul className="flex flex-col space-y-2">
-                            <li className="text-2xl font-bold">
-                                <a href="#first-section">Text here</a>
-                            </li>
-                            <li className="text-2xl font-bold">
-                                <a href="#">Text here</a>
-                            </li>
-                            <li className="text-2xl font-bold">
-                                <a href="#">Text here</a>
-                            </li>
-                            <li className="text-2xl font-bold">
-                                <a href="#">Text here</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+            {/* Assuming `Template` is a component you have defined */}
+            {/* Make sure to replace it with the actual component you want to use */}
+            <Template links={links} BannerImage={"https://cdn.pixabay.com/photo/2016/09/01/19/53/pocket-watch-1637396_1280.jpg"} Title={"Getting Started with Programming"} Posts={<Firstpost/>} Others={"link"} />
+            <div className="mt-4">
+                <Pagination />
             </div>
-            <div className=" h-full w-full md:w-1/2 mt-4 rounded-2xl">
-                <PostBanner imageUrl="https://cdn.pixabay.com/photo/2016/09/01/19/53/pocket-watch-1637396_1280.jpg"/>
-                <div className="flex justify-between items-center my-2 mx-6 ">
-                    <h1 className="text-4xl font-bold ">
-                        Text here
-                    </h1>
-                    <div>
-                        12/12/2023
-                    </div>
-                </div>
-                {/*Get teh name from url*/}
-                <div className="p-6 mt-10 bg-slate-600 rounded-2xl text-white">
-                    <Firstpost/>
-                    <BlogHeader/>
-                    <BlogHeader/>
-                    <BlogHeader/>
-                    <BlogHeader/>
-                    <div id="first-section"></div>
-                    <BlogHeader/>
-                </div>
-            </div>
-            <div className="bg-slate-600 rounded-2xl h-screen w-1/4 mt-4">
-                <div className="p-4">
-c
-                </div>
-            </div>
-        </div>
-        <div className="mt-4">
-            <Pagination/>
-        </div>
         </div>
     );
 }
 
-export default page;
+export default Page;
